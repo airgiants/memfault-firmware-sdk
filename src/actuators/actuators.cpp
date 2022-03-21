075bb10a2472f20905b837cb6c35474c8e4d3304
Robert Nixdorf <rob@rnix.de>
2022-03-21 23:04:37 +0000

    tidy

--- src/actuators/actuators.cpp
+++ src/actuators/actuators.cpp
@@ -25,45 +25,20 @@ void Actuators::begin(){
 
     board.enable_power();
 
+    inflate.set(0);
     inflate.set_servo(PIN_SERVO0);
-    inflate.set(50);
+    deflate.set(180);
     deflate.set_servo(PIN_SERVO1);
-    deflate.set(50);
-    
-
 }
 
 void Actuators::update(){
-    const int period = 50;
+    const int period = 125;
     static unsigned long prev = 0;
     unsigned long const now = millis();
     if(now - prev > period) {
-
         inflate.update();
         deflate.update();
-        
         prev = now;
     }
 
-
 }
-// void setupServo() {
-// 	// Allow allocation of all timers
-// 	ESP32PWM::allocateTimer(0);
-// 	ESP32PWM::allocateTimer(1);
-// 	ESP32PWM::allocateTimer(2);
-// 	ESP32PWM::allocateTimer(3);
-
-// 	//set output puffer to output
-
-    
-// 	inflateServo.setPeriodHertz(50);    // standard 50 hz servo
-// 	deflateServo.setPeriodHertz(50);    // standard 50 hz servo
-// 	inflateServo.attach(PIN_SERVO2, 1100, 2000); // attaches the servo on pin 18 to the servo object
-// 	deflateServo.attach(PIN_SERVO1, 1100, 2000); // attaches the servo on pin 18 to the servo object
-// 	// using default min/max of 1000us and 2000us
-// 	// different servos may require different min/max settings
-// 	// for an accurate 0 to 180 sweep
-//     inflateServo.write(180.0);
-//     deflateServo.write(0.0);
-// }
